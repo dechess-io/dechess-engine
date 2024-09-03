c509736610668468cae0714c659dc45ac570300a
Natrix <76524304+luongtrieuvy202@users.noreply.github.com>
2024-09-03 18:18:13 +0700

    fix: add bot

--- src/services/bot.ts
+++ src/services/bot.ts
@@ -4,4 +4,4 @@ import TelegramBot from "node-telegram-bot-api";
 const token = "7327954703:AAFrrU8HoojYN4m6UQxb9x6Oe0fjDDO4Dp8";
 
 // Create a bot instance
-export const bot = null
+export const bot = new TelegramBot(token, { polling: true });
