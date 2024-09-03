cf9a96c643c977c644cba6457c3c8427b8039a81
Natrix <76524304+luongtrieuvy202@users.noreply.github.com>
2024-09-03 22:53:38 +0700

    fix: update bot token

--- src/services/bot.ts
+++ src/services/bot.ts
@@ -1,7 +1,7 @@
 import TelegramBot from "node-telegram-bot-api";
 
 // Replace with your bot token
-const token = "7327954703:AAFrrU8HoojYN4m6UQxb9x6Oe0fjDDO4Dp8";
+const token = "7327954703:AAFWceo5wQtQ2Qbbf7iQJhua9o2cReQ7_to";
 
 // Create a bot instance
 export const bot = new TelegramBot(token, { polling: true });
